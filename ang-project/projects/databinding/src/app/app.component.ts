a01f0b6cd06d9a234ea8a57508dd7df0c21d3d4c
vaibhav5996k <vibhukatkar96@gmail.com>
2020-10-04 02:22:45 +0530

    home work done

--- ang-project/projects/databinding/src/app/app.component.ts
+++ ang-project/projects/databinding/src/app/app.component.ts
@@ -8,4 +8,46 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'databinding';
   brd = '1px solid yellow';
+  fntwt = '500';
+  counter = 0;
+
+  clk(btn: number) {
+    if (btn == 1) {
+      this.fntwt += 250;
+    } else {
+      this.fntwt += 250;
+    }
+  }
+
+  dblclick() {
+    return this.counter++;
+  }
+
+  drag() {
+    return this.counter++;
+  }
+
+  dragover() {
+    return this.counter++;
+  }
+
+  mousedown() {
+    return this.counter++;
+  }
+
+  mouseup() {
+    return this.counter++;
+  }
+
+  mouseenter() {
+    return this.counter++;
+  }
+
+  mouseleave() {
+    return this.counter++;
+  }
+
+  mouseover() {
+    return this.counter++;
+  }
 }
